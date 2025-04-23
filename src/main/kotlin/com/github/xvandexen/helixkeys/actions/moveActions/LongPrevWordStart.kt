862e052c24bbedf45f21375b45d59e2bf8d3fb11
Xvandexen <Ben.Mcinally@proton.me>
2025-04-23 08:47:11 +0100

    Adds Extend_Line.kt and alters focus detection

--- src/main/kotlin/com/github/xvandexen/helixkeys/actions/moveActions/LongPrevWordStart.kt
+++ src/main/kotlin/com/github/xvandexen/helixkeys/actions/moveActions/LongPrevWordStart.kt
@@ -11,7 +11,7 @@ class LongPrevWordStart: AnAction() {
 
   override fun actionPerformed(e: AnActionEvent) {
     val components = getEditorComponents(e.project!!)?: return
-    val (caretModel, _, _, text) = components
+    val (caretModel, _, selectionModel, text) = components
     val currentPosition = caretModel.offset
 
     var newOffset = currentPosition
@@ -23,6 +23,7 @@ class LongPrevWordStart: AnAction() {
     }
 
     if (newOffset != currentPosition) {
+      selectionModel.setSelection(currentPosition, newOffset)
       caretModel.moveToOffset(newOffset)
     }
 
---
+++ src/main/kotlin/com/github/xvandexen/helixkeys/services/Registry.kt
@@ -0,0 +1,41 @@
+package com.github.xvandexen.helixkeys.services
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import java.util.concurrent.ConcurrentHashMap
+
+@Service(Service.Level.PROJECT)
+class Registry(private val project: Project) {
+  data class Register(
+    var anchor: Int? = null,
+    val selectionHistory: MutableList<Pair<Int,Int>> = mutableListOf()
+  ) {
+    fun updateAnchor(offset: Int) {
+      anchor = offset;
+    }
+
+    fun addSelectionToHistory(start: Int, end: Int) {
+      selectionHistory.add(Pair(start, end))
+
+    }
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): Registry = project.service()
+  }
+
+  private val editorsAndRegisters = ConcurrentHashMap<Editor, Register>()
+
+  fun getRegister(editor: Editor): Register? {
+    return editorsAndRegisters.get(editor)
+  }
+
+  fun addRegistry(editor: Editor){
+    editorsAndRegisters.put(editor,Register())
+  }
+
+
+}
